c7aee2616ba36fcce237bdcfbdba59c3f246af20
ye <1403975138@qq.com>
2024-03-25 18:55:13 +0800

    initial

---
+++ src/BasicStruct/TreeNode.java
@@ -0,0 +1,94 @@
+package BasicStruct;
+
+import java.util.*;
+
+public class TreeNode {
+    int val;
+
+    TreeNode left;
+
+    TreeNode right;
+
+    TreeNode(int val){
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    /**层序遍历
+     * 本质是广度优先遍历(breadth-first traversal)*/
+    List<Integer> levelOrder(TreeNode root){
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        List<Integer> list = new ArrayList<>();
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            list.add(node.val);
+            if(node.left != null){
+                queue.offer(node.left);
+            }
+            if(node.right != null){
+                queue.offer(node.right);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 前序遍历
+     * 本质是深度遍历，下同(depth-first traversal)
+     * 访问优先级：根节点 -> 左子树 -> 右子树
+     */
+    void preOrder(TreeNode root, List<Integer> list){
+        if(root == null) return;
+
+        list.add(root.val);
+        preOrder(root.left, list);
+        preOrder(root.right, list);
+    }
+
+    /**
+     * 中序遍历
+     * 访问优先级：左子树 -> 根节点 -> 右子树
+     */
+    void inOrder(TreeNode root, List<Integer> list){
+        if(root == null) return;
+
+        inOrder(root.left, list);
+        list.add(root.val);
+        inOrder(root.right, list);
+    }
+
+    /**
+     * 后序遍历
+     * 访问优先级：左子树 -> 右子树 -> 根节点
+     */
+    void postOrder(TreeNode root, List<Integer> list){
+        if(root == null) return;
+
+        postOrder(root.left, list);
+        postOrder(root.right, list);
+        list.add(root.val);
+    }
+
+    /**
+     * 查找节点
+     */
+    TreeNode search(TreeNode root, int num){
+        TreeNode cur = root;
+
+        while(cur != null){
+            if(cur.val < num){
+                cur = cur.right;
+            }else if(cur.val> num){
+                cur = cur.left;
+            }else break;
+        }
+        return cur;
+    }
+}
