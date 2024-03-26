ff8f8d74e316318ec096f6bc4da65b82564560e2
ye <1403975138@qq.com>
2024-03-26 22:03:45 +0800

    Array, List, Stack, queue, dequeue

---
+++ src/BasicStruct/DoubleListNode.java
@@ -0,0 +1,13 @@
+package BasicStruct;
+
+public class DoubleListNode {
+    int val;
+
+    DoubleListNode prev;
+
+    DoubleListNode next;
+
+    DoubleListNode(int val){
+        this.val = val;
+    }
+}
