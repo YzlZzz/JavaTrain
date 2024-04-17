f51a453192e182359b75959dc7a1e78e660f40ef
ye <1403975138@qq.com>
2024-04-17 14:54:23 +0800

    interview

--- src/Interview/Xhs.java
+++ src/Interview/Xhs.java
@@ -1,8 +1,6 @@
 package Interview;
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.*;
 
 
 /**
@@ -52,6 +50,50 @@ public class Xhs {
             ans = Math.max(ans, temp.comment * sum);
         }
         System.out.println(ans);
+    }
+
+    public static int maxScoreDp(int n, int k, int[] likes, int[] comments) {
+        List<Pair<Integer, Integer>> blogs = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            blogs.add(new Pair<>(likes[i], comments[i]));
+        }
+        blogs.sort((a, b) -> b.getKey() - a.getKey());
+
+        int[][] dp = new int[n + 1][k + 1];
+        int[] minComments = new int[n + 1];
+        Arrays.fill(minComments, Integer.MAX_VALUE);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= Math.min(i, k); j++) {
+                minComments[j] = Math.min(minComments[j], blogs.get(i - 1).getValue());
+                dp[i][j] = dp[i - 1][j];
+                if (j == 1) {
+                    dp[i][j] = Math.max(dp[i][j], blogs.get(i - 1).getKey() * minComments[j]);
+                } else {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + blogs.get(i - 1).getKey() * minComments[j]);
+                }
+            }
+        }
+
+        return dp[n][k];
+    }
+
+    private static class Pair<T, U> {
+        private T key;
+        private U value;
+
+        public Pair(T key, U value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public T getKey() {
+            return key;
+        }
+
+        public U getValue() {
+            return value;
+        }
     }
 }
 
